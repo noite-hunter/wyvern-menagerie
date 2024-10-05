30ff53181f0faa965a048ddd35e7e2ecd78988f8
Noite <140098495+noite-hunter@users.noreply.github.com>
2024-10-05 20:16:37 -0300

    Finished card styling

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,7 +3,10 @@ import styles from "./Card.module.css";
 export function Card({ monster }: { monster: string | null }) {
   return (
     <div className={styles.monsterCard}>
-      <div className={styles.thumbnailContainer}></div>
+      <div className={styles.thumbnailBackground}>
+        <div className={styles.thumbnailEffect}></div>
+        <div className={styles.thumbnailContainer}></div>
+      </div>
       <div className={styles.nameContainer}>
         <span className={styles.monsterName}>{monster}</span>
       </div>
