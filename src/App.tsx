6e13ebbc77f7c9d44e6c766e4392e254af34ee31
Noite <140098495+noite-hunter@users.noreply.github.com>
2024-10-05 21:16:35 -0300

    Finished main page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ function App() {
   const monsters: (string | null)[][] = monstersJson
   const filteredMonsters: string[] = monsters.flat().filter((item): item is string => !!item)
 
-  console.log(filteredMonsters);
+  const findId = (name: string): number => monsters.findIndex((row) => row.includes(name))
+  const findSubId = (name: string): number => monsters.find((row) => row.includes(name))?.indexOf(name) ?? -1
   
   return (
     <>
@@ -21,7 +22,7 @@ function App() {
         </header>
         <div className={styles.cardSpread}>
           {filteredMonsters.map((monster, index) => (
-            <Card key={index} monster={monster}/>
+            <Card key={index} monster={monster} monsterIndex={findId(monster)} variationIndex={findSubId(monster)}/>
           ))}
         </div>
         <footer><h2>{filteredMonsters.length + ' Large Monsters'}</h2></footer>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,23 @@
 import styles from "./Card.module.css";
 
-export function Card({ monster }: { monster: string | null }) {
+type CardProps = {
+  monster: string,
+  monsterIndex: number,
+  variationIndex: number
+}
+
+export function Card({ monster, monsterIndex, variationIndex }: CardProps) {
+  const thumbnailUrl: string = '/src/assets/thumbs/thumb-' +
+    (monsterIndex + 1).toString().padStart(3, '0') +
+    '_' +
+    variationIndex.toString().padStart(2, '0') +
+    '.png';  
+
   return (
     <div className={styles.monsterCard}>
       <div className={styles.thumbnailBackground}>
-        <div className={styles.thumbnailEffect}></div>
-        <div className={styles.thumbnailContainer}></div>
+        <div className={styles.thumbnailEffect} style={{backgroundImage: `url(${thumbnailUrl})`}}></div>
+        <div className={styles.thumbnailContainer} style={{backgroundImage: `url(${thumbnailUrl})`}}></div>
       </div>
       <div className={styles.nameContainer}>
         <span className={styles.monsterName}>{monster}</span>
